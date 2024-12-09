4b542f6a2cf201ca9e974ce0ee4b74fd4d91b944
klokssser <74603800+klokssser@users.noreply.github.com>
2024-12-09 12:41:25 +0500

    Update pythashki.h

--- pythashki.h
+++ pythashki.h
@@ -1,1 +1,15 @@
-#pragma once
+#ifndef	PYTHASKI_H
+#define PYTHASKI_H
+
+#include <array>
+#include "startAndPrint.cpp"
+#include "swapElements.cpp"
+#include "victoryAndMain.cpp"
+
+void swapElements(array<array<int, 4>, 4>& base, int& i0, int& j0, int& trys);
+void hideCursor();
+array<array<int, 4>, 4> startGame(int& i0, int& j0);
+void printGame(const array<array<int, 4>, 4>& mass);
+inline bool victory(array<array<int, 4>, 4>& base);
+
+#endif
