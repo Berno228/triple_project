95ffd94b8bb61307a1dfba8431f6e288b623b8f3
klokssser <74603800+klokssser@users.noreply.github.com>
2024-12-15 20:26:13 +0500

    Update victoryAndMain.cpp

--- victoryAndMain.cpp
+++ victoryAndMain.cpp
@@ -1,41 +1,37 @@
-﻿#include <iostream>
+#include <iostream>
 #include <array>
 #include <string>
 #include <Windows.h>
-using namespace std;
+#include "pythashki.h"
 
-void swapElements(array<array<int, 4>, 4>& base, int& i0, int& j0, int& trys);
-void hideCursor();
-array<array<int, 4>, 4> startGame(int& i0, int& j0);
-void printGame(const array<array<int, 4>, 4>& mass);
-
-bool victory(array<array<int, 4>, 4>& base) {
-    return (base == array<array<int, 4>, 4>{(1, 2, 3, 4), (5, 6, 7, 8), (9, 10, 11, 12), (13, 14, 15, 0)});
+bool victory(std::array<std::array<int, 4>, 4>& base) {
+    return (base == std::array<std::array<int, 4>, 4>{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0});
 }//проверка текущего состояния на победу
 
 
 int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
+
 	hideCursor();
 
 	std::string s;
 	while (s != "нет") {
 		int i0{}, j0{}, trys{};
-		array<array<int, 4>, 4> base = startGame(i0, j0);
+		std::array<std::array<int, 4>, 4> base = startGame(i0, j0);
 
 		swapElements(base, i0, j0, trys);
 
-		cout << "Вы выиграли поздравляю!\n"
+		std::cout << "Вы выиграли поздравляю!\n"
 			"кол-во клеток, которые пришлось передвинуть: " << trys << '\n'
 			<< "Желаете сыграть еще раз введите (да/нет): ";
-		cin >> s;
+		std::cin >> s;
 
 		//проверка ввода
-		while (s != "нет" || s != "да") {
-			cout << "Вы ввели некорректное значение, повторите ввод.";
-			cin.ignore(32000, '\n');
-			cin >> s;
+		while (!(s == "нет" || s == "да")) {
+			std::cout << "Вы ввели некорректное значение, повторите ввод.\n";
+			std::cin.ignore(32000, '\n');
+			std::cin >> s;
 		}
 	}
 
